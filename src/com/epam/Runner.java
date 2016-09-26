4e7a780615d4da5680a6e3191ea5161c0b2536f7
Viachaslau Skuratau <Viachaslau_Skuratau@epam.com>
2016-09-26 15:51:48 +0300

    add new line

--- src/com/epam/Runner.java
+++ src/com/epam/Runner.java
@@ -1,10 +1,8 @@
 package com.epam;
 
-/**
- * Created by Viachaslau_Skuratau on 9/26/2016.
- */
 public class Runner {
     public static void main (String [] args) {
         System.out.println("test");
+        System.out.println("new line");
     }
 }
