b4ef4ae6b765e2be8a8ea56314d3bb6db3f1a787
Viachaslau Skuratau <Viachaslau_Skuratau@epam.com>
2016-09-26 15:25:13 +0300

    Initial commit

---
+++ src/com/epam/Runner.java
@@ -0,0 +1,10 @@
+package com.epam;
+
+/**
+ * Created by Viachaslau_Skuratau on 9/26/2016.
+ */
+public class Runner {
+    public static void main (String [] args) {
+        System.out.println("test");
+    }
+}
